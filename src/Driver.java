edd376cb8f89f79ce3f234d13ea5e2fe99a370f8
Harvey Lallave <harvey_lallave@dlsu.edu.ph>
2021-04-09 18:29:16 +0800

    Completed all of the functions

--- src/Driver.java
+++ src/Driver.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 
 public class Driver {
 
+
+	private static final int[] DENOMINATION_VALUE = {20, 10, 5, 2, 1};
+	private static final int   DENOMINATION_SIZE  = 5;
 	
-	private int[] denominations = {0, 0, 0, 0, 0, 0};
+	private static int[] denomination = {0, 0, 0, 0, 0};
 	
 	public static void main(String[] args) {
-
-		System.out.println(isNum("123"));
-		System.out.println(isNum("123.2"));
-		System.out.println(isNum("ab"));
-		System.out.println(isNum("1a2"));
-		System.out.println(isNum("12-3"));
+ 
 		String input;
 		Scanner sc = new Scanner(System.in);
 		
 		while (true) {
-			input = sc.nextLine().toUpperCase();
+			input = sc.nextLine().toLowerCase();
 			
-			if(input.equals("QUIT"))
+			if(input.equals("quit"))
 				break;
 			
 			String[] strArr = input.split(" ");
@@ -27,11 +25,11 @@ public class Driver {
 			if(strArr[0].equals("show") && validateInput(strArr, 1))
 				displayDenomination();
 
-			else if(strArr[0].equals("put") && validateInput(strArr, 6))
-				putDenomination(strArr);
+			else if(strArr[0].equals("put") && validateInput(strArr, DENOMINATION_SIZE + 1))
+				updateDenomination(strArr, true);
 
-			else if(strArr[0].equals("take") && validateInput(strArr, 6))
-				takeDenomination(strArr);
+			else if(strArr[0].equals("take") && validateInput(strArr, DENOMINATION_SIZE + 1))
+				updateDenomination(strArr, false);
 
 			else if(strArr[0].equals("change") && validateInput(strArr, 2))
 				getChange(strArr);
@@ -41,27 +39,69 @@ public class Driver {
 	}
 
 	private static void displayDenomination() {
-		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
 		
+		sb.append("$" + getTotalAmount());
+		for(int val : denomination)
+			sb.append(" " + val);
+		
+		System.out.println(sb.toString());
 	}
 
-	private static void putDenomination(String[] strArr) {
-		// TODO Auto-generated method stub
+	private static int getTotalAmount() {
+		int total = 0;
 		
+		for(int i = 0; i < DENOMINATION_SIZE; i++)
+			total += denomination[i] * DENOMINATION_VALUE[i];
+		
+		return total;
 	}
 
-	private static void takeDenomination(String[] strArr) {
-		// TODO Auto-generated method stub
-		
+	private static void updateDenomination(String[] strArr, boolean add) {
+		if(add)
+			 for(int i = 1; i < DENOMINATION_SIZE + 1; i++)
+				 denomination[i - 1] += Integer.parseInt(strArr[i]);
+		else for(int i = 1; i < DENOMINATION_SIZE + 1; i++)
+				denomination[i - 1] -= Integer.parseInt(strArr[i]);
+		 
+		displayDenomination();
 	}
-
+ 
 	private static void getChange(String[] strArr) {
-		// TODO Auto-generated method stub
+		int[] changeDenomination = new int[DENOMINATION_SIZE];
+		int toChange = Integer.parseInt(strArr[1]);
+		int total = getTotalAmount();
 		
+		if(toChange > total)
+			System.out.println("Sorry, not enough change.");
+		else {
+			for(int i = 0; i < DENOMINATION_SIZE; i++)
+				while(toChange > DENOMINATION_VALUE[i] && 
+						denomination[i] > 0) {
+					int div = toChange / DENOMINATION_VALUE[i];
+					
+					toChange -= DENOMINATION_VALUE[i] * div;
+					denomination[i] -= div;
+					
+					changeDenomination[i] = div;
+				} 
+
+			System.out.println("ToChange: " + toChange);
+			for(int i = 0; i < DENOMINATION_SIZE; i++)
+				System.out.print((i == 0 ? "" : " ") + changeDenomination[i]);
+			if(toChange != 0) 
+				System.out.println("Sorry, not enough change.");
+			else {
+				for(int i = 0; i < DENOMINATION_SIZE; i++)
+					System.out.print((i == 0 ? "" : " ") + changeDenomination[i]);
+				System.out.println();
+			}
+				
+		}
 	}
 
 	private static void invalidInput() {
-		System.out.println("Please enter a valid input.\n");
+		System.out.println("Please enter a valid input.");
 	}
 	
 	private static boolean validateInput(String[] strArr, int length) {
