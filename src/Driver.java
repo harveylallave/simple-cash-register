40a5efdbaf514a456cb3fe3493888111b266611c
Harvey Lallave <harvey_lallave@dlsu.edu.ph>
2021-04-09 17:48:50 +0800

    Added validation of inputs

--- src/Driver.java
+++ src/Driver.java
@@ -8,6 +8,11 @@ public class Driver {
 	
 	public static void main(String[] args) {
 
+		System.out.println(isNum("123"));
+		System.out.println(isNum("123.2"));
+		System.out.println(isNum("ab"));
+		System.out.println(isNum("1a2"));
+		System.out.println(isNum("12-3"));
 		String input;
 		Scanner sc = new Scanner(System.in);
 		
@@ -19,16 +24,16 @@ public class Driver {
 			
 			String[] strArr = input.split(" ");
 			 
-			if(strArr[0].equals("show") && strArr.length == 1)
+			if(strArr[0].equals("show") && validateInput(strArr, 1))
 				displayDenomination();
 
-			else if(strArr[0].equals("put") && strArr.length == 6)
+			else if(strArr[0].equals("put") && validateInput(strArr, 6))
 				putDenomination(strArr);
 
-			else if(strArr[0].equals("take") && strArr.length == 6)
+			else if(strArr[0].equals("take") && validateInput(strArr, 6))
 				takeDenomination(strArr);
 
-			else if(strArr[0].equals("change") && strArr.length == 2)
+			else if(strArr[0].equals("change") && validateInput(strArr, 2))
 				getChange(strArr);
 			
 			else invalidInput();
@@ -58,5 +63,20 @@ public class Driver {
 	private static void invalidInput() {
 		System.out.println("Please enter a valid input.\n");
 	}
+	
+	private static boolean validateInput(String[] strArr, int length) {
+		if(length != strArr.length)
+			return false;
+		
+		else if(length > 1)
+			for(int i = 1; i < length; i++)
+				if(!isNum(strArr[i]))
+					return false;
+		
+		return true;
+	}
 
+	private static boolean isNum(String string) {
+		return string.matches("\\d+");
+	}
 }
